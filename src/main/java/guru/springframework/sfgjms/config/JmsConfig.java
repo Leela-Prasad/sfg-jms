eaff5da2af2d16d2f6b91720f901e19d282132c6
Leela <leela.jagu@gmail.com>
2019-09-23 22:30:03 +0530

    Adding Message Convertor to convert any object to JMS Text Message - closes #3

---
+++ src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        // Converts objects to JMS Text Message
+        converter.setTargetType(MessageType.TEXT);
+        // This property will carry the Type id.
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
